5a15d9970a59cd99913fb39f082719edbebca059
Darren542 <Darrenluck@icloud.com>
2025-02-10 19:56:56 -0800

    updated map data

--- mapme-frontend/src/App.tsx
+++ mapme-frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Sidebar from "./components/Sidebar";
 import { CesiumProvider } from "./components/CesiumContext/CesiumProvider";
 import { AddonProvider } from "./components/AddonManagerContext/AddonProvider";
 import AddonManager from "./components/AddonManager";
 
 const App = () => {
   return (
+    // TODO Set these to be rendered after someone logs in.
     <CesiumProvider>
       <AddonProvider>
         <div style={{ display: "flex", height: "calc(100vh - 60xpx)" }}>
