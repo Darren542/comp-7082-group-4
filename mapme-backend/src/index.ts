6a058a66a17297c21d8f6c89d7bee997a48fd211
Darren542 <Darrenluck@icloud.com>
2025-03-13 22:01:53 -0700

    Users Addon Status saved on Server

--- mapme-backend/src/index.ts
+++ mapme-backend/src/index.ts
@@ -3,19 +3,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth";
+import addonRoutes from "./routes/addon";
+
 import { mockData } from "./mock";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-
-function updateAddon(id: string, active: boolean, installed: boolean) {
-  const addon = mockData.installedAddons.find((addon) => addon.id === id);
-  if (addon) {
-    addon.active = active;
-    addon.installed = installed;
-  }
-}
 
 const app: Express = express();
 app.use(cors());
@@ -27,16 +21,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/auth", authRoutes); 
 
-app.get("/api/addons", (req, res) => {
-  // Mock installed data
-  res.json(mockData.installedAddons);
-});
-
-app.post("/api/addons", (req, res) => {
-  const { id, active, installed } = req.body;
-  updateAddon(id, active, installed);
-  res.json(mockData.installedAddons);
-});
+app.use("/api/addons", addonRoutes);
 
 app.listen(PORT, () => {
   console.log(`Backend is running on http://localhost:${PORT}`);
