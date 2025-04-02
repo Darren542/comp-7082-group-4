fd2652488a9059907a587aad2b5fe58ba7d7cf2c
Darren542 <Darrenluck@icloud.com>
2025-04-01 21:07:09 -0700

    Initial Playwright Test

--- mapme-frontend/src/components/button.tsx
+++ mapme-frontend/src/components/button.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 type ButtonProps = {
+  id?: string;
   text: string;
   onClick: () => void;
 };
 
 //button component
-export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({ id, text, onClick }) => {
+  // Remove Whitespace from text to use as id if id is not provided
+  if (!id) {
+    id = text.replace(/\s+/g, "").toLowerCase();
+  }
   return (
     <button
+      data-testid={id}
+      id={id ?? undefined}
       onClick={onClick}
       className="cursor-pointer bg-[#5465FF] text-[#E2FDFF] rounded-full px-10 py-3 text-lg hover:bg-[#4252cc] transition"
     >
